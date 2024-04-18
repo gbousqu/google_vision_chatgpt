e10eb03b1b449d36b9decd100feafb1baa3aa8b5
gbousqu <gilles.bousquet@9online.fr>
2024-04-18 13:31:47 +0200

    Update import statements in app.py to fix module compatibility issue

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 
-import pip
+import sys
+
+import subprocess
+import sys
 
 try:
     import google.cloud
 except ModuleNotFoundError:
-    pip.install('google-cloud-vision')  # Remplacez par la bibliothèque spécifique dont vous avez besoin
+    subprocess.call(["pip", "install", "google-cloud-vision"])  # Replace with the specific library you need
+
+    # Add this line to handle potential compatibility issues
+    if 'google.cloud' not in sys.modules:
+        import google.cloud  # Explicitly import after installation
+
 
 from google.cloud import vision_v1
 
